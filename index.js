9952b6da6f8d9e7af2cc35321fc18f3a1f2ea4aa
harithzamri <harithtoikee96@gmail.com>
2020-09-01 09:47:47 +0800

    simple message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const Discord = require("discord.js");
+
+const { prefix, token } = require("./config.json");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Ready");
+});
+
+client.on("message", (message) => {
+  message.channel.send("hello").then((message) => console.log(message.content));
+});
+
+client.login(token);
