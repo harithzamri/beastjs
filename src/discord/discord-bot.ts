c1f67719c4fc34cff00075a0733069139d45f097
harithzamri <harithtoikee96@gmail.com>
2021-03-17 08:44:27 +0800

    add seperate files for discord connection

---
+++ src/discord/discord-bot.ts
@@ -0,0 +1,49 @@
+import Discord from "discord.js";
+import type { Client as DiscordClient } from "discord.js";
+import { getLogger } from "../utils/logger";
+
+const logger = getLogger({
+  name: "Beast-bot",
+});
+
+export async function createDiscordClientImp(
+  resolve: (dc: DiscordClient) => void
+): Promise<void> {
+  const client = new Discord.Client();
+
+  client.on("message", (message) => {
+    // eslint-disable-next-line max-len
+    logger.debug(
+      `[DISCORD MSG] USER:'${message.author.id}' CHANNEL:'${message.channel.id}' CONTENT:'${message.content}'`
+    );
+    if (message.content === "!ping") {
+      void message.channel.send("pong!");
+    }
+  });
+
+  client.on("messageUpdate", (oldMessage, newMessage) => {
+    const userId = newMessage.author ? newMessage.author.id : "unknown";
+    // eslint-disable-next-line max-len
+    logger.debug(
+      `[DISCORD MSG UPDATE] USER:'${userId}' CHANNEL:'${
+        newMessage.channel.id
+      }' CONTENT:'${String(newMessage.content)}'`
+    );
+  });
+
+  const loginResult = await client.login(process.env.token);
+  if (loginResult !== process.env.token) {
+    logger.warn("login return value does not match");
+  }
+}
+
+export function createDiscordClient(): Promise<DiscordClient> {
+  return new Promise<Discord.Client>((resolve, rejects) => {
+    try {
+      void createDiscordClientImp(resolve);
+    } catch (error) {
+      logger.error("Failed to create Discord Client:" + String(error));
+      rejects(error);
+    }
+  });
+}
