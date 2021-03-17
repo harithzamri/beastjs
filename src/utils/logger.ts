4dbd822f2f5d70f6e2122c472ed02abba87493f6
harithzamri <harithtoikee96@gmail.com>
2021-03-17 08:45:05 +0800

    add seperate file for utils/loggers

---
+++ src/utils/logger.ts
@@ -0,0 +1,11 @@
+import { Logger, LogLevel } from "@d-fischer/logger";
+
+export function getLogger({ name }: { name: string }): Logger {
+  return new Logger({
+    name,
+    minLevel: LogLevel.DEBUG,
+    colors: true,
+    emoji: true,
+    timestamps: true,
+  });
+}
