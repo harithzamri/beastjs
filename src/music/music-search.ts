c6fa412128ce4612d9f8203cbca92d4ffc970ecc
harithzamri <harithtoikee96@gmail.com>
2021-03-18 08:47:03 +0800

    try with add twitch feature in the bot

--- src/music/music-search.ts
+++ src/music/music-search.ts
@@ -1,25 +1,27 @@
 import { getLogger } from "../utils/logger";
 import { YouTube } from "youtube-sr";
+import { SimpleTrack } from "./music-track";
 
 const logger = getLogger({
   name: "Beast-Music-Search",
 });
 
-const settings = {
-  prefix: "!",
-  token: "Your Discord Token",
-};
-
 export async function searchMusic(): Promise<any> {
-  let tracks: string[] = [];
+  return new Promise(async (resolve) => {
+    let tracks: any[] = [];
+    await YouTube.search("hello")
+      .then((results) => {
+        if (results && results.length !== 0) {
+          logger.info("Searching for lyrics");
+          tracks = results.map((r) => new SimpleTrack(r));
+          if (tracks.length === 0) return logger.error("no songs");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(tracks);
 
-  YouTube.search("despacito")
-    .then((results) => {
-      if (results && results.length !== 0) {
-        tracks = results.map((r) => ne);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    return resolve(tracks[0]);
+  });
 }
--- src/music/music-track.ts
+++ src/music/music-track.ts
@@ -1,19 +1,23 @@
-interface Track {
-  videoData: string;
-  user: string;
-  player: string;
-  fromPlaylist?: boolean;
+interface VideoData {
+  url: string;
+  duration: number;
+  title: string;
 }
 
 export class SimpleTrack {
-  private _videoData: string;
-  private _user: string;
-  private _player: string;
-  private _fromPlaylist: boolean;
+  private _url: string;
+  private _duration: number;
+  private _title: string;
 
-  constructor({ videoData, user, player, fromPlaylist = true }: Track) {
-    this._videoData = videoData;
-    this._user = user;
-    (this._player = player), (this._fromPlaylist = fromPlaylist);
+  constructor(videoData: any) {
+    this._url = videoData.url;
+
+    this._duration =
+      videoData.durationFormatted ||
+      `${Math.floor(parseInt(videoData.lenghtSeconds) / 60)}:${
+        parseInt(videoData.lenghtSeconds) % 60
+      }`;
+
+    this._title = videoData.title;
   }
 }
