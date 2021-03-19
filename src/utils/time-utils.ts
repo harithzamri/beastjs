175cfe4e88285884190b2c928ae371dfeec89f9f
harithzamri <harithtoikee96@gmail.com>
2021-03-19 08:38:38 +0800

    create time utils so that it can be reusable

---
+++ src/utils/time-utils.ts
@@ -0,0 +1,9 @@
+export function refreshed(
+  lastUse: Date | undefined,
+  cooldownMs: number
+): boolean {
+  if (!lastUse) {
+    return true;
+  }
+  return Number(new Date()) - Number(lastUse) > cooldownMs;
+}
