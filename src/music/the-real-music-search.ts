63453efedbcd0505d597e6641717b28ef7dfb479
harithzamri <harithtoikee96@gmail.com>
2021-03-22 08:40:44 +0800

    edit on class music search

--- src/music/the-real-music-search.ts
+++ src/music/the-real-music-search.ts
@@ -1,19 +1,22 @@
 import type { Client as DiscordClient } from "discord.js";
+import { Collection, Snowflake } from "discord.js";
 import { Logger } from "@d-fischer/logger";
 import EventEmitter from "node:events";
 import { getLogger } from "src/utils/logger";
 import { MusicUtils } from "../utils/music-utils";
+import { SimpleQueue } from "./music-queue";
 
 export interface DiscordConfig {
   discordClient: DiscordClient;
   options: {};
 }
 
-class PlayerSearch extends EventEmitter {
+class SimpleSearch extends EventEmitter {
   private _discordClient: DiscordClient;
   private _logger: Logger;
   private _options: {};
   private _util: MusicUtils;
+  private _queus = new Collection<Snowflake, SimpleQueue>();
 
   constructor({ discordClient, options }: DiscordConfig) {
     super();
@@ -25,7 +28,9 @@ class PlayerSearch extends EventEmitter {
       name: "beast-bot-searching-tracks",
     });
 
-    this._util = new MusicUtils;
+    this._util = new MusicUtils();
+
+    this._queus = new Collection();
     
   }
 }
