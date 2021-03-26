e7726abe6a364206bf87ed856e469f615106d2e5
harithzamri <harithtoikee96@gmail.com>
2021-03-26 09:40:12 +0800

    refactor the ffmpeg into utils

--- src/utils/music-utils.ts
+++ src/utils/music-utils.ts
@@ -3,6 +3,21 @@ import { YouTube } from "youtube-sr";
 export class MusicUtils {
   constructor() {}
 
+  public checkFFMPEG() {
+    try {
+    } catch (error) {
+      this.alertFFMPEG();
+      return false;
+    }
+  }
+
+  public alertFFMPEG() {
+    console.log(
+      chalk.red("ERROR:"),
+      'FFMPEG is not installed. Install with "npm install ffmpeg-static" or download it here: https://ffmpeg.org/download.html.'
+    );
+  }
+
   public isYTPlaylistLink(query: string) {
     return YouTube.validate(query, "PLAYLIST_ID");
   }
