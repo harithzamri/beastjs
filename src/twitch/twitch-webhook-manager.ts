11547d5afda576b9b05a828fd7d2ee7e41959ee4
harithzamri <harithtoikee96@gmail.com>
2021-04-07 09:28:47 +0800

    modified to add status cache

--- src/twitch/twitch-webhook-manager.ts
+++ src/twitch/twitch-webhook-manager.ts
@@ -1,11 +1,57 @@
 import type { ChatClient } from "twitch-chat-client/lib";
-import type { ApiClient } from "twitch";
+import type { ApiClient, HelixStream } from "twitch";
 import type { Client as DiscordClient } from "discord.js";
 import type { ConnectCompatibleApp } from "twitch-webhooks";
 import { EnvPortAdapter, WebHookListener } from "twitch-webhooks";
+import { Logger } from "@d-fischer/logger/lib";
+import { getLogger } from "src/utils/logger";
+import { TwitchStreamStatus } from "./twitch-stream-status-cache";
 
 export interface TwitchWebHookManagerConfig {
   apiClient: ApiClient;
   chatClient: ChatClient;
   discordClient: DiscordClient;
+}
+
+function getStreamStatus(stream: HelixStream | undefined): TwitchStreamStatus {
+  return stream ? TwitchStreamStatus.LIVE : TwitchStreamStatus.OFFLINE;
+}
+
+export class TwitchWebhookManager {
+  private _apiClient: ApiClient;
+  private _chatClient: ChatClient;
+  private _discordClient: DiscordClient;
+  private _webhook: WebHookListener;
+  private _logger: Logger;
+
+  constructor({
+    apiClient,
+    chatClient,
+    discordClient,
+  }: TwitchWebHookManagerConfig) {
+    this._apiClient = apiClient;
+
+    this._chatClient = chatClient;
+    this._discordClient = discordClient;
+
+    this._webhook = new WebHookListener(
+      apiClient,
+      new EnvPortAdapter({
+        hostName: "example.com",
+        port: 8080,
+      }),
+      {
+        logger: {
+          name: "twitch-webhook-listener",
+          timestamps: true,
+          minLevel: "DEBUG",
+          colors: false,
+        },
+      }
+    );
+
+    this._logger = getLogger({
+      name: "reabeastbot-twitch-webhook-manager",
+    });
+  }
 }
